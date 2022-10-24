d522c169cac591731201a64a96a83d479d016113
EmmanuelRamosBiado <ramosbiadoe@gmail.com>
2022-10-24 14:12:29 +0200

    Primer Commit

---
+++ Ejercicios PHP - 6/02.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['numeroOculto'])) {
+    $_SESSION['numeroOculto'] = random_int(1, 20);
+    $_SESSION['intentos'] = 5;
+    echo "<h1> !! BIENVENIDO !! </h1>";
+    echo "Oportinudades restantes: " . $_SESSION['intentos'];
+} else {
+    if ($_SESSION['numeroOculto'] == $_REQUEST['numeroUsuario']) {
+        echo "<h1> Enhorabunea, has acertado </h1> <br><br>";
+        session_destroy();
+        header("Refresh:5");
+    } else {
+        echo "No has acertado, prueba otra vez <br><br>";
+        echo ("Oportinudades restantes: " . $_SESSION['intentos']);
+    }
+
+    if ($_SESSION['intentos'] == 0) {
+        echo "<h1> Se acabaron tus intentos, has perdido. </h1>";
+        session_destroy();
+        header("Refresh:5");
+    }
+}
+?>
+
+<form method="get">
+    Introduce un numero: <input type="text" name="numeroUsuario">
+    <?php $_SESSION['intentos']--; ?>
+</form>
